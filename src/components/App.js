1f709c8da9acd5a85217e0854fdab665fb2ea55a
SAM <sameersharma12@gmail.com>
2025-11-25 07:27:10 +0530

    Implement sum calculator in src/ components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [numbers, setNumbers] = useState([])
+  const [sum, setSum]=useState(0)
+  const [input, setInput]=useState("")
+
+  useEffect(()=>{
+    const timer= setTimeout(()=>{
+      setSum(numbers.reduce((acc,curr)=>acc+curr,0));
+    },0)
+
+    return ()=> clearTimeout(timer)
+  },[numbers])
+
+
+  const handleInput=(e)=>{
+    const value= e.target.value;
+    setInput(value);
+
+    if(value !=="" && !isNaN(Number(value))){
+      setNumbers((prev)=>[...prev, Number(value)])
+    }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input 
+        type="number"
+        value={input}
+        onChange={handleInput}
+       />
+      <p>Sum: {sum}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
